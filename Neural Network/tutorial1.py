9c67c23689ae2617a3517605195a4c5c5a37ce33
Steve <statco19@gmail.com>
2021-06-22 15:40:26 +0900

    text classification

--- Neural Network/tutorial1.py
+++ Neural Network/tutorial1.py
@@ -12,11 +12,26 @@ data = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# rescale 0~255 into 0~1
+# rescale 0~255 into 0~1 (pre-processing)
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-print(train_images[7])
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
 
-plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
+
